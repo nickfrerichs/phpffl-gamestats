8e40675f222fa0fe0cd2c962d0023ba79248cdef
Nick Frerichs <frerichs@gmail.com>
2015-10-18 16:33:23 -0500

    Remove config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 # This is your phpFFL database, access can be read-only, it's only used it to compare player names, positions and teams
 # to try to determine a players nflID.
-dbhost = 'localhost'
-dbuser = 'root'
+dbhost = ''
+dbuser = ''
 dbpass = ''
-dbname = 'phpffl2014'
+dbname = ''
